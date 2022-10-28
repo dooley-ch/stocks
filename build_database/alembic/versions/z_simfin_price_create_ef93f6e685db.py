bf1f17b85ce1d92935e47cb2c88881128b376aeb
James Dooley <james@developernotes.org>
2022-10-28 15:21:58 +0200

    Removed support for imported prices

--- build_database/alembic/versions/z_simfin_price_create_ef93f6e685db.py
+++ build_database/alembic/versions/z_simfin_price_create_ef93f6e685db.py
@@ -17,25 +17,7 @@ depends_on = None
 
 
 def upgrade() -> None:
-    op.create_table('z_simfin_price',
-                    sa.Column('id', sa.Integer(), primary_key=True, autoincrement=True),
-                    sa.Column('ticker', sa.String(length=12), nullable=False),
-                    sa.Column('simfin_id', sa.Integer(), nullable=False),
-                    sa.Column('price_date', sa.String(length=50), nullable=True),
-                    sa.Column('open_price', sa.String(length=50), nullable=True),
-                    sa.Column('low_price', sa.String(length=50), nullable=True),
-                    sa.Column('high_price', sa.String(length=50), nullable=True),
-                    sa.Column('close_price', sa.String(length=50), nullable=True),
-                    sa.Column('adjusted_close_price', sa.String(length=50), nullable=True),
-                    sa.Column('dividend', sa.String(length=50), nullable=True),
-                    sa.Column('volume', sa.String(length=50), nullable=True),
-                    sa.Column('shares_outstanding', sa.String(length=50), nullable=True))
-
-    op.create_index("z_simfin_price_ticker", "z_simfin_price", ["ticker"])
-    op.create_index("z_simfin_price_simfin_id", "z_simfin_price", ["simfin_id"])
-
+    pass
 
 def downgrade() -> None:
-    op.drop_index("z_simfin_price_ticker", "z_simfin_price")
-    op.drop_index("z_simfin_price_simfin_id", "z_simfin_price")
-    op.drop_table("z_simfin_price")
+    pass
